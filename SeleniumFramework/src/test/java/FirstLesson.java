ebe0bb30582f614efae9aad0e70698b2364d1c22
jr <jr@jrs-macbook-air.lan>
2020-04-30 19:40:53 -0500

    firstPush

---
+++ SeleniumFramework/src/test/java/FirstLesson.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstLesson {
+	
+	public static void main(String[] args) {
+		
+		//WebDriver Driver = new ChromeDriver();
+		//Driver.get("TEST");
+		
+
+		
+	}
+
+}
